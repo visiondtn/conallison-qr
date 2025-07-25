a280855e46f5fa6de35dbbba8c02f410569d9b42
Agustin Rodriguez <agus.rodriguez777@gmail.com>
2025-07-25 17:50:26 -0300

    feat: renovar suscripcion

--- app/page.js
+++ app/page.js
@@ -1,17 +1,32 @@
-import Body from '@/components/Body'
-import { Footer } from '@/components/Footer'
-import Header from '@/components/Header'
-import Image from 'next/image'
-
+import Body from "@/components/Body";
+import { Footer } from "@/components/Footer";
+import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <main className="flex items-center justify-center min-h-screen bg-gray-900">
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <Image
+            src={"/assets/warning.png"}
+            alt="Logo"
+            width={150}
+            height={150}
+            quality={40}
+            loading="lazy"
+            className="text-center text-white"
+          />
+          <p className="text-center text-xl font-normal">
+            Es necesario renovar la suscripción
+          </p>
+        </div>
+      </main>
+      {/* <Header />
       <main>
         <Body />
       </main>
-      <Footer />
+      <Footer /> */}
     </>
-  )
+  );
 }
